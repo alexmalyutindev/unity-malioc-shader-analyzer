e390881966d848ae5603b6bc3b83945c5c157777
Alexander Malyutin <alex.malyutin.dev@gmail.com>
2025-06-30 21:01:44 +0400

    Initial commit

---
+++ Editor/Report.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using J = Newtonsoft.Json.JsonPropertyAttribute;
+
+namespace MaliOC.Editor
+{
+    [Serializable]
+    public class Report
+    {
+        [J("producer")] public Producer Producer;
+        [J("schema")] public Schema Schema;
+        [J("shaders")] public ShaderElement[] Shaders;
+    }
+
+    [Serializable]
+    public class Producer
+    {
+        [J("build")] public string Build;
+        [J("documentation")] public Uri Documentation;
+        [J("name")] public string Name;
+        [J("version")] public long[] Version;
+    }
+
+    [Serializable]
+    public class Schema
+    {
+        [J("name")] public string Name;
+        [J("version")] public long Version;
+    }
+
+    [Serializable]
+    public class ShaderElement
+    {
+        [J("driver")] public string Driver;
+        [J("filename")] public string Filename;
+        [J("hardware")] public Hardware Hardware;
+        [J("notes")] public string[] Notes;
+        [J("properties")] public ShaderProperty[] Properties;
+        [J("shader")] public ShaderShader Shader;
+        [J("variants")] public Variant[] Variants;
+        [J("warnings")] public string[] Warnings;
+    }
+
+    [Serializable]
+    public class Hardware
+    {
+        [J("architecture")] public string Architecture;
+        [J("core")] public string Core;
+        [J("pipelines")] public Pipeline[] Pipelines;
+        [J("revision")] public string Revision;
+    }
+
+    [Serializable]
+    public class Pipeline
+    {
+        [J("description")] public string Description;
+        [J("display_name")] public string DisplayName;
+        [J("name")] public string Name;
+    }
+
+    [Serializable]
+    public class ShaderProperty
+    {
+        [J("description")] public string Description;
+        [J("display_name")] public string DisplayName;
+        [J("name")] public string Name;
+        [J("value")] public bool Value;
+    }
+
+    [Serializable]
+    public class ShaderShader
+    {
+        [J("api")] public string Api;
+        [J("type")] public string Type;
+    }
+
+    [Serializable]
+    public class Variant
+    {
+        [J("name")] public string Name;
+        [J("performance")] public Performance Performance;
+        [J("properties")] public VariantProperty[] Properties;
+    }
+
+    [Serializable]
+    public class Performance
+    {
+        [J("longest_path_cycles")] public Cycles LongestPathCycles;
+        [J("pipelines")] public string[] Pipelines;
+        [J("shortest_path_cycles")] public Cycles ShortestPathCycles;
+        [J("total_cycles")] public Cycles TotalCycles;
+    }
+
+    [Serializable]
+    public class Cycles
+    {
+        [J("bound_pipelines")] public string[] BoundPipelines;
+        [J("cycle_count")] public double[] CycleCount;
+    }
+
+    [Serializable]
+    public class VariantProperty
+    {
+        [J("description")] public string Description;
+        [J("display_name")] public string DisplayName;
+        [J("name")] public string Name;
+        [J("value")] public Value Value;
+    }
+
+    [Serializable]
+    public struct Value
+    {
+        public bool? Bool;
+        public long? Integer;
+
+        public static implicit operator Value(bool Bool) => new Value { Bool = Bool };
+        public static implicit operator Value(long Integer) => new Value { Integer = Integer };
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                ValueConverter.Singleton,
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class ValueConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(Value) || t == typeof(Value?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonToken.Integer:
+                    var integerValue = serializer.Deserialize<long>(reader);
+                    return new Value { Integer = integerValue };
+                case JsonToken.Boolean:
+                    var boolValue = serializer.Deserialize<bool>(reader);
+                    return new Value { Bool = boolValue };
+            }
+
+            throw new Exception("Cannot unmarshal type Value");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            var value = (Value)untypedValue;
+            if (value.Integer != null)
+            {
+                serializer.Serialize(writer, value.Integer.Value);
+                return;
+            }
+
+            if (value.Bool != null)
+            {
+                serializer.Serialize(writer, value.Bool.Value);
+                return;
+            }
+
+            throw new Exception("Cannot marshal type Value");
+        }
+
+        public static readonly ValueConverter Singleton = new ValueConverter();
+    }
+}
---
+++ Editor/ShaderAnalyzerTool.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Unity.EditorCoroutines.Editor;
+using UnityEditor;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Debug = UnityEngine.Debug;
+using Shader = UnityEngine.Shader;
+
+namespace MaliOC.Editor
+{
+    public class ShaderAnalyzerTool : EditorWindow
+    {
+        [SerializeField] private Shader _shader;
+        [SerializeField] private Report _report;
+
+        private Vector2 _definesScrollPosition;
+        private Vector2 _mainScrollPosition;
+
+        private UnityEditor.Editor _inspector;
+        private EditorCoroutine _maliOCProcessHandle;
+        private GUIStyle _fieldLabelStyle;
+
+        private readonly Dictionary<string, bool> _keywords = new();
+
+        [MenuItem("Tools/" + nameof(ShaderAnalyzerTool))]
+        public static void Create()
+        {
+            CreateWindow<ShaderAnalyzerTool>();
+        }
+
+        private void OnSelectionChange()
+        {
+            if (Selection.activeObject is Shader shader)
+            {
+                _shader = shader;
+                UnityEditor.Editor.CreateCachedEditor(_shader, typeof(ShaderInspector), ref _inspector);
+                Repaint();
+
+                Debug.Log(_shader);
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (_shader == null) return;
+            if (_inspector is not ShaderInspector shaderInspector) return;
+
+            shaderInspector.DrawHeader();
+
+            using var scrollViewScope = new GUILayout.ScrollViewScope(_mainScrollPosition);
+            _mainScrollPosition = scrollViewScope.scrollPosition;
+
+            if (_maliOCProcessHandle == null)
+            {
+                using (new EditorGUILayout.VerticalScope(GUILayout.Height(300)))
+                {
+                    using var scrollViewScope2 = new GUILayout.ScrollViewScope(_definesScrollPosition);
+                    _definesScrollPosition = scrollViewScope2.scrollPosition;
+
+                    foreach (var localKeyword in _shader.keywordSpace.keywords)
+                    {
+                        if (!_keywords.TryGetValue(localKeyword.name, out var value))
+                        {
+                            value = false;
+                            _keywords[localKeyword.name] = false;
+                        }
+
+                        using (var changeScope = new EditorGUI.ChangeCheckScope())
+                        {
+                            var newValue = GUILayout.Toggle(value, localKeyword.name);
+                            if (changeScope.changed)
+                            {
+                                _keywords[localKeyword.name] = newValue;
+                            }
+                        }
+                    }
+                }
+
+                if (GUILayout.Button("Analyze"))
+                {
+                    AnalyzeShader(_shader, _keywords.Where(pair => pair.Value).Select(pair => pair.Key).ToArray());
+                }
+            }
+            else
+            {
+                if (GUILayout.Button("Cancel"))
+                {
+                    EditorCoroutineUtility.StopCoroutine(_maliOCProcessHandle);
+                    _maliOCProcessHandle = null;
+                    Debug.Log("Canceled");
+                }
+            }
+
+            if (_report != null)
+            {
+                DrawReport(_report);
+            }
+        }
+
+        private void DrawReport(Report report)
+        {
+            // Header
+            {
+                GUILayout.BeginHorizontal();
+                var v = report.Producer.Version;
+                EditorGUILayout.LabelField(
+                    $"{report.Producer.Name} v{v[0]}.{v[1]}.{v[2]} Build ({report.Producer.Build})");
+                if (EditorGUILayout.LinkButton("Documentation"))
+                {
+                    Application.OpenURL(report.Producer.Documentation.AbsoluteUri);
+                }
+
+                GUILayout.EndHorizontal();
+            }
+            EditorGUILayout.Space(5);
+
+            _fieldLabelStyle = new GUIStyle(EditorStyles.boldLabel)
+            {
+                padding = new RectOffset(20, 0, 0, 0),
+            };
+
+            foreach (var shader in report.Shaders)
+            {
+                EditorGUILayout.LabelField("Shader file:", shader.Filename, EditorStyles.wordWrappedLabel);
+
+                EditorGUILayout.LabelField("Configuration:");
+                {
+                    using (new GUILayout.VerticalScope(EditorStyles.frameBox))
+                    {
+                        EditorGUILayout.LabelField("Hardware:", $"{shader.Hardware.Core} {shader.Hardware.Revision}");
+                        EditorGUILayout.LabelField("Architecture:", shader.Hardware.Architecture);
+                        EditorGUILayout.LabelField("Driver:", shader.Driver);
+                        EditorGUILayout.LabelField("Shader type:", $"{shader.Shader.Api} {shader.Shader.Type}");
+                    }
+                }
+
+                GUILayout.Space(5);
+
+                foreach (var variant in shader.Variants)
+                {
+                    EditorGUILayout.LabelField("Shader:", variant.Name);
+                    using (new EditorGUILayout.VerticalScope(EditorStyles.frameBox, GUILayout.Width(300)))
+                    {
+                        foreach (var variantProperty in variant.Properties)
+                        {
+                            GUIContent valueContent;
+                            if (variantProperty.Value.Bool.HasValue)
+                            {
+                                valueContent = new GUIContent(variantProperty.Value.Bool.Value ? "True" : "False");
+                            }
+                            else
+                            {
+                                var format = variantProperty.Name switch
+                                {
+                                    "fp16_arithmetic" or "thread_occupancy" or "fp16_idle_lanes" => @"0\%",
+                                    _ => "",
+                                };
+                                valueContent = new GUIContent(variantProperty.Value.Integer?.ToString(format));
+                            }
+
+                            EditorGUILayout.LabelField(
+                                new GUIContent(variantProperty.DisplayName, variantProperty.Description),
+                                valueContent,
+                                _fieldLabelStyle
+                            );
+                        }
+                    }
+
+                    DrawPerformanceTable(variant.Performance, shader.Hardware);
+                }
+
+                EditorGUILayout.Space(5);
+
+                EditorGUILayout.LabelField("Shader properties:");
+                using (new GUILayout.VerticalScope(EditorStyles.frameBox, GUILayout.Width(300)))
+                {
+                    foreach (var shaderProperty in shader.Properties)
+                    {
+                        EditorGUILayout.LabelField(
+                            new GUIContent(shaderProperty.DisplayName, shaderProperty.Description),
+                            new GUIContent(shaderProperty.Value ? "True" : "False"),
+                            _fieldLabelStyle
+                        );
+                    }
+                }
+
+                GUILayout.Label("Notes:");
+                foreach (var note in shader.Notes)
+                {
+                    GUILayout.Label(note);
+                }
+
+                GUILayout.Label("Warnings:");
+                foreach (var warning in shader.Warnings)
+                {
+                    GUILayout.Label(warning);
+                }
+            }
+        }
+
+        private static void DrawPerformanceTable(Performance performance, Hardware hardware)
+        {
+            GUILayout.Label("Performance:");
+            using (new GUILayout.HorizontalScope(EditorStyles.frameBox))
+            {
+                EditorGUILayout.BeginVertical();
+                GUILayout.Label("Cycles");
+                GUILayout.Label("Total");
+                GUILayout.Label("Shortest");
+                GUILayout.Label("Longest");
+                EditorGUILayout.EndVertical();
+
+                for (var i = 0; i < performance.Pipelines.Length; i++)
+                {
+                    EditorGUILayout.BeginVertical();
+                    var pipeline = performance.Pipelines[i];
+                    var pipelineInfo = hardware.Pipelines.First(p => p.Name == pipeline);
+                    GUILayout.Label(new GUIContent(pipelineInfo.DisplayName, pipelineInfo.Description));
+
+                    GUILayout.Label(performance.TotalCycles.CycleCount[i].ToString("F2"));
+                    GUILayout.Label(performance.ShortestPathCycles.CycleCount[i].ToString("F2"));
+                    GUILayout.Label(performance.LongestPathCycles.CycleCount[i].ToString("F2"));
+                    EditorGUILayout.EndVertical();
+                }
+
+                using (new EditorGUILayout.VerticalScope())
+                {
+                    GUILayout.Label("Bound By");
+
+                    EditorGUILayout.BeginHorizontal();
+                    foreach (var boundPipeline in performance.TotalCycles.BoundPipelines)
+                    {
+                        var boundName = hardware.Pipelines.First(p => p.Name == boundPipeline).DisplayName;
+                        GUILayout.Label(boundName);
+                    }
+
+                    EditorGUILayout.EndHorizontal();
+
+                    EditorGUILayout.BeginHorizontal();
+                    foreach (var boundPipeline in performance.ShortestPathCycles.BoundPipelines)
+                    {
+                        var boundName = hardware.Pipelines.First(p => p.Name == boundPipeline).DisplayName;
+                        GUILayout.Label(boundName);
+                    }
+
+                    EditorGUILayout.EndHorizontal();
+
+                    EditorGUILayout.BeginHorizontal();
+                    foreach (var boundPipeline in performance.LongestPathCycles.BoundPipelines)
+                    {
+                        var boundName = hardware.Pipelines.First(p => p.Name == boundPipeline).DisplayName;
+                        GUILayout.Label(boundName);
+                    }
+                }
+
+                EditorGUILayout.EndVertical();
+            }
+        }
+
+        private void AnalyzeShader(Shader sourceShader, string[] keywords)
+        {
+            // TODO: Configure Shader variant!
+            var shaderData = new ShaderData(sourceShader);
+            var shaderPassData = shaderData
+                .GetSubshader(0)
+                .GetPass(0)
+                .CompileVariant(
+                    ShaderType.Fragment,
+                    keywords,
+                    ShaderCompilerPlatform.Vulkan,
+                    BuildTarget.Android,
+                    GraphicsTier.Tier1,
+                    forExternalTool: true
+                )
+                .ShaderData;
+
+            var tempPath = $"Temp/{sourceShader.name.Replace('/', '_')}.frag";
+            File.WriteAllBytes(tempPath, shaderPassData);
+
+            _maliOCProcessHandle = EditorCoroutineUtility.StartCoroutine(
+                MakePassReport(tempPath, json =>
+                {
+                    try
+                    {
+                        _report = Newtonsoft.Json.JsonConvert.DeserializeObject<Report>(json);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogException(e);
+                    }
+
+                    Repaint();
+                    _maliOCProcessHandle = null;
+                }),
+                this
+            );
+        }
+
+        private IEnumerator MakePassReport(string shaderPassFile, Action<string> onComplete)
+        {
+            // TODO: Use vulkan only if target API is Vulkan!
+            // TODO: Add OpenGLES support!
+
+            yield return RunMaliOC(
+                shaderPassFile,
+                new[] { "--vulkan", "--format json" },
+                (output, error) =>
+                {
+                    Debug.Log(output);
+                    if (!string.IsNullOrEmpty(error)) Debug.LogError(error);
+                    onComplete?.Invoke(output);
+                }
+            );
+
+            yield return RunMaliOC(
+                shaderPassFile,
+                new[] { "--vulkan" },
+                (output, error) =>
+                {
+                    Debug.Log(output);
+                    if (!string.IsNullOrEmpty(error)) Debug.LogError(error);
+                }
+            );
+        }
+
+        private IEnumerator RunMaliOC(string inputFilePath, string[] additionalArgs, Action<string, string> onComplete)
+        {
+            var args = $"{string.Join(' ', additionalArgs)} \"{inputFilePath}\"";
+            Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo("malioc", args)
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                }
+            };
+
+            Debug.Log($"{process.StartInfo.FileName} {process.StartInfo.Arguments}");
+            var output = new StringBuilder();
+            var error = new StringBuilder();
+
+            process.OutputDataReceived += (_, dataEvent) => output.AppendLine(dataEvent.Data);
+            process.ErrorDataReceived += (_, dataEvent) => error.AppendLine(dataEvent.Data);
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            while (!process.HasExited)
+            {
+                yield return null;
+            }
+
+            var errorLog = error.ToString().Trim();
+            onComplete?.Invoke(output.ToString(), errorLog);
+        }
+    }
+}
